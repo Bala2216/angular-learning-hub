14ea1f6ea727c31960717603f849df1da8ca9a35
vinothkannaui <v.srinivasan@perficient.com>
2025-09-04 16:56:07 +0530

    updated the name using onInit

--- lifecyclehook/src/app/app.component.ts
+++ lifecyclehook/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 
 @Component({
@@ -7,6 +7,18 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.user.name = 'Kanna';
+    }, 2000);
+  }
+
   title = 'Lifecyclehook';
+
+  user = {
+    name: 'Vinoth',
+    gender: 'Male',
+    age: 30,
+  };
 }
