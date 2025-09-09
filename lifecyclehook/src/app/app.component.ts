1105f8e62a6e1ba61e2f5505f6908c4c94fb12c6
vinothkannaui <v.srinivasan@perficient.com>
2025-09-09 11:09:13 +0530

    rm unwanted code

--- lifecyclehook/src/app/app.component.ts
+++ lifecyclehook/src/app/app.component.ts
@@ -18,14 +18,4 @@ export class AppComponent {
     date: string;
     status: string;
   }[] = [];
-
-  onUserAdded(newUser: {
-    name: string;
-    phone: number;
-    age: number;
-    date: string;
-    status: string;
-  }) {
-    this.users.push(newUser);
-  }
 }
