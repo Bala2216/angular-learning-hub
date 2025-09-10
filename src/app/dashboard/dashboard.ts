b3321031bc14a4e025cc9da454145f3cbec50dde
cnarayanaswamy <Ci.Narayanaswamy@perficient.com>
2025-09-10 12:01:16 +0530

    code formatting

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthStore } from '../services/auth';
 import { CommonModule } from '@angular/common';
-import { FormRendererComponent } from "../form-renderer/form-renderer";
-import { Onboarding } from "../employee/onboarding/onboarding";
 import { Directives } from "../directives/directives";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ CommonModule, FormRendererComponent, Onboarding, Directives],
+  imports: [ CommonModule, Directives],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
