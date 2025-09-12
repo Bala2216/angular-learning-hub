2924355bec882007f4b44a3c088351c098ea2e9d
vinothkannaui <v.srinivasan@perficient.com>
2025-09-12 18:19:55 +0530

    search input styles

--- lifecyclehook/src/app/user-card/user-card.component.ts
+++ lifecyclehook/src/app/user-card/user-card.component.ts
@@ -2,16 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../interfaces/user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-card',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   providers: [UserService],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
 export class UserCardComponent {
   users: User[] = [];
+  searchText: string = '';
 
   constructor(private userService: UserService) {
     this.userService.getUsers().subscribe((data) => {
