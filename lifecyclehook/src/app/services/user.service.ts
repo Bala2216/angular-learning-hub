f9a90eff871ee207065fe2b0c89005fa5bb7c72c
vinothkannaui <v.srinivasan@perficient.com>
2025-09-12 13:11:22 +0530

    List using service

--- lifecyclehook/src/app/services/user.service.ts
+++ lifecyclehook/src/app/services/user.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private apiURL = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>(this.apiURL);
+  }
 }
--- lifecyclehook/src/app/user-card/user-card.component.ts
+++ lifecyclehook/src/app/user-card/user-card.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-card',
-  imports: [],
+  imports: [CommonModule],
+  providers: [UserService],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
 })
-export class UserCardComponent {}
+export class UserCardComponent {
+  users: any[] = [];
+
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+}
