7d7968293ed034b5f8d68e423ca7f1cdbb33f949
vinothkannaui <v.srinivasan@perficient.com>
2025-09-15 15:02:05 +0530

    filter functionality

--- lifecyclehook/src/app/user-card/user-card.component.ts
+++ lifecyclehook/src/app/user-card/user-card.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { User } from '../interfaces/user';
 import { FormsModule } from '@angular/forms';
+import { UserFilterPipe } from '../pipes/user-filter.pipe';
 
 @Component({
   selector: 'app-user-card',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, UserFilterPipe],
   providers: [UserService],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.scss',
